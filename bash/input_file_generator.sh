b00014795bcfff413aa866f2a9197b569fa9c8fe
Lane <love.allsciences@gmail.com>
2018-07-12 18:50:20 -0500

    fixed the input file generator

--- bash/input_file_generator.sh
+++ bash/input_file_generator.sh
@@ -4,4 +4,4 @@ cd ../lammps_runs/
 
 temperature=$1
 
-replaced=$(grep -l "replace_here" template.in | xargs sed "s/replace_here/$temperature/g" template.in | tee "$temperature.in")
+replaced=$(grep -l "replace_here" template.in | xargs sed "s/replace_here/$temperature/g" > "$temperature.in")
