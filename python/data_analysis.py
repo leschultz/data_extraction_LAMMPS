4b70e46b012aa79fcdf7db3d96362d06ec0d256a
Lane <love.allsciences@gmail.com>
2018-07-12 20:10:44 -0500

    improved the python script functionality

--- python/data_analysis.py
+++ python/data_analysis.py
@@ -12,31 +12,35 @@ help_temperature_anneal = (
         )
 
 parser.add_argument('-n',
-                     type=int,
-                     help=help_temperature_anneal,
+                    type=int,
+                    nargs='+',
+                    help=help_temperature_anneal,
                     )
 
 args = parser.parse_args()
 
-data = pd.read_csv(
-        '../data/'+str(args.n)+'.txt',
-                   comment='#',
-                   sep=' ',
-                   skiprows=1,
-                   header=None
-                   )
+for item in args.n:
+    data = pd.read_csv(
+                       '../data/'+str(item)+'.txt',
+                       comment='#',
+                       sep=' ',
+                       skiprows=1,
+                       header=None
+                       )
 
-data.columns = ([
-                 'time step',
-                 'temperature',
-                 'pressure',
-                 'volume',
-                 'potential energy',
-                 'kinetic energy'
-                 ])
+    data.columns = ([
+                     'time step',
+                     'temperature',
+                     'pressure',
+                     'volume',
+                     'potential energy',
+                     'kinetic energy'
+                     ])
 
-pl.plot(data['time step'], data['temperature'])
-pl.xlabel('Time Step [fs]')
-pl.ylabel('Temperature [K]')
-pl.grid(True)
-pl.savefig('../images/'+str(args.n))
+    pl.plot(data['time step'], data['temperature'])
+    pl.xlabel('Time Step [fs]')
+    pl.ylabel('Temperature [K]')
+    pl.legend([str(item)+' [K] annealing'])
+    pl.grid(True)
+    pl.savefig('../images/'+str(item))
+    pl.clf()
