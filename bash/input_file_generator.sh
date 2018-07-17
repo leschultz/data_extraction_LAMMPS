58457783cf7a7a87181c3491a81015bba2ef839d
Lane <love.allsciences@gmail.com>
2018-07-17 09:13:20 -0500

    fixed a bug

--- bash/input_file_generator.sh
+++ bash/input_file_generator.sh
@@ -11,6 +11,6 @@ for ((i=1;i<=$number_runs;i++))
 do
 	for var in "$@"
 	do
-		replaced=$(grep -l "replace_here" template.in | xargs sed "s/replace_here/$var/g; s/replace_seed/$RANDOM/g; s/replace_atom_number/$atom_number/g" > "$var"'K_'"$i.in")
+		replaced=$(grep -l "replace_temperature" template.in | xargs sed "s/replace_temperature/$var/g; s/replace_seed/$RANDOM/g; s/replace_atom_number/$atom_number/g" > "$var"'K_'"$i.in")
 	done
 done
