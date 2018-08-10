1285c4ebb122cbea340d0a7182a5e63d06dd3ced
Lane <love.allsciences@gmail.com>
2018-08-10 17:11:18 -0500

    analysis is a work in progress

--- python/analysis.py
+++ python/analysis.py
@@ -25,7 +25,7 @@ class analize(object):
 		self.frq = self.tem[0]  # Acquistion Frequency (steps/acqusition)
 		self.trj = self.tem[1]  # Trajectories
 
-	def vibration(self, start, plot=True):
+	def vibration(self, start, stop, plot=True):
 		'''
 		This function calculates vibration displacements.
 		'''
@@ -46,35 +46,55 @@ class analize(object):
 		if start % freq == 1:
 			raise NameError('Start is not a multiple of the data acquisition rate')
 
+		# Check if end goes beyond data range
+		if stop > last_step:
+			raise NameError('Cannot gather more data than specified')
+
+		# Check if end is valid point
+		if stop % freq == 1:
+			raise NameError('End is not a multiple of the data acqusition rate')
+
 		# Find the displacements due to vibration between each timestep
 		disp_mean = []
 		step_recorded = []
-		for i in range(start, last_step, freq):
-			index0 = data.index[data.step == i].tolist()
-			index1 = data.index[data.step == i+freq].tolist()
+		x = []
+		y = []
+		z = []
+		for i in range(start, stop+1, freq):
+			index = data.index[data.step == i].tolist()
 			
-			x0 = data.xu[index0].values  # Past x positions
-			x1 = data.xu[index1].values  # Future x positions
-			dx = difference(x1, x0)  # Difference in x coordinates
-			
-			y0 = data.yu[index0].values  # Past y positions
-			y1 = data.yu[index1].values  # Future y positions
-			dy = difference(y1, y0)  # Difference in y coordinates
+			x.append(data.xu[index].values.tolist())  # x positions
+			y.append(data.yu[index].values.tolist())  # y positions
+			z.append(data.zu[index].values.tolist())  # z positions
 
-			z0 = data.zu[index0].values  # Past z positions
-			z1 = data.zu[index1].values  # Future z positions
-			dz = difference(z1, z0)  # Difference in z coordinates
-
-			disp = displacement(dx, dy, dz)  # Displacement for each atom
-
-			disp_mean.append(np.mean(disp))  # Mean displacement for all steps
 			step_recorded.append(i)  # Time step
 
+		# Averages between start and stop
+		x_mean = np.mean(x, axis=0)  # Average x values
+		y_mean = np.mean(y, axis=0)  # Average y values
+		z_mean = np.mean(z, axis=0)  # Average z values
+
+		mean_coordinates = zip(x_mean, y_mean, z_mean)
+
+		# Take displacement of the averages with respect to (0,0,0)
+		mean_positions = []
+		for item in mean_coordinates:
+			mean_positions.append((item[0]**2+item[1]**2+item[2]**2)**0.5)
+
+		for i in range(start, stop+1, freq):
+			index = data.index[data.step == i].tolist()
+
+			x = data.xu[index].values  # x values for each atom
+			y = data.yu[index].values  # y values for each atom
+			z = data.zu[index].values  # z values for each atom
+
+		'''
 		if plot == True:
 			pl.plot(step_recorded, disp_mean)
+			pl.xlim([start, stop])
 			pl.xlabel('Step [-]')
 			pl.ylabel('Vibration [A]')
 			pl.legend([run])
 			pl.grid(True)
 			pl.tight_layout()
-			pl.show()
+			pl.show()'''
