5892c237d652cf61b7634bf03f0e07934e326c69
Lane <love.allsciences@gmail.com>
2018-08-27 13:39:13 -0500

    Removed useless line.

--- python/ovito_calc.py
+++ python/ovito_calc.py
@@ -123,9 +123,6 @@ def rdfcalc(name, frame, cut, bins):
     modifier = CoordinationNumberModifier(cutoff=cut, number_of_bins=bins)
     node.modifiers.append(modifier)
 
-    # Initialize array for accumulated RDF historgram to zero
-    total_rdf = np.zeros((modifier.number_of_bins, 2))
-
     # Compute RDF of the current frame
     out = node.compute(frame)
 
