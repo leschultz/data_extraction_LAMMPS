aa2af08ee65dc2138c8b241ec546645806bc6cf6
Lane <love.allsciences@gmail.com>
2018-09-02 14:31:26 -0500

    Added a directory maker for bash.

---
+++ bash/directories.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create the needed directories for LAMMPS
+cd ../data/
+mkdir -p dat
+mkdir -p lammpstrj
+mkdir -p rest
+mkdir -p txt
--- bash/lammps_looper.sh
+++ bash/lammps_looper.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Create the needed directories for LAMMPS
-cd ../data/
-mkdir -p dat
-mkdir -p lammpstrj
-mkdir -p rest
-mkdir -p txt
-
 # Change to inputfile directory
 cd ../input_files/
 
