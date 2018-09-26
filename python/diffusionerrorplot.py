33ad5f3ec3d8a76d02edac0034a4306ca0c8c390
Lane <love.allsciences@gmail.com>
2018-09-26 11:32:57 -0500

    working on error still

---
+++ python/diffusionerrorplot.py
@@ -0,0 +1,125 @@
+from atom_error_propagation import diffusion as di
+from block_averaging import block_averaging as bl
+from matplotlib import pyplot as pl
+from diffusionimport import load
+from scipy import stats as st
+
+import pandas as pd
+import numpy as np
+import os
+
+directory = '../datacalculated/diffusion/'
+
+runs = os.listdir(directory)
+
+diffusion = {}
+diffusion['scipy'] = []
+diffusion['block'] = []
+diffusion['sem'] = []
+differr = {}
+differr['scipy'] = []
+differr['block'] = []
+differr['fit'] = []
+differr['sem'] = []
+
+temp = []
+temp2 = []
+for run in runs:
+
+    if '_origins' not in run:
+
+        temp.append(int(run.split('run1_')[1]))
+
+        with open(directory+run) as file:
+            next(file)
+            for line in file:
+                value = line.strip().split(',')
+                diffusion['scipy'].append(float(value[0]))
+                differr['scipy'].append(float(value[1]))
+
+    if '_origins' in run:
+
+        word = run.split('run1_')[1]
+        number = int(word.split('_')[1])
+        temp2.append(number)
+
+        data = load(directory+run, ' ')
+        block = bl(data)
+        diffusion['block'].append(block['all'])
+        differr['block'].append(block['all_err'])
+
+        diffusion['sem'].append(np.mean(data['all']))
+        differr['sem'].append(st.sem(data['all']))
+
+directory = '../datacalculated/msd/'
+
+runs = os.listdir(directory)
+
+for run in runs:
+    data = load(directory+run, ',')
+    atom = di(data)
+    differr['fit'].append(atom['all_err'])
+
+fig, axs = pl.subplots(2, 2)
+
+sizemarker = 2
+
+axs[0, 0].errorbar(
+                   temp,
+                   diffusion['scipy'],
+                   differr['scipy'],
+                   marker='.',
+                   markersize = sizemarker,
+                   linestyle='None',
+                   ecolor='r'
+                   )
+
+axs[1, 0].errorbar(
+                   temp,
+                   diffusion['scipy'],
+                   differr['fit'],
+                   marker='.',
+                   markersize = sizemarker,
+                   linestyle='None',
+                   ecolor='r'
+                   )
+
+axs[0, 1].errorbar(
+                   temp2,
+                   diffusion['block'],
+                   differr['block'],
+                   marker='.',
+                   markersize = sizemarker,
+                   linestyle='None',
+                   ecolor='r'
+                   )
+
+axs[1, 1].errorbar(
+                   temp2,
+                   diffusion['sem'],
+                   differr['sem'],
+                   marker='.',
+                   markersize = sizemarker,
+                   linestyle='None',
+                   ecolor='r'
+                   )
+
+axs[0, 0].set_xlabel('test')
+xlabel = 'Temperature [K]'
+ylabel = 'Diffusion [*10^-4 cm^2 s^-1]'
+
+axs[0, 0].set_title('Scipy fit error')
+axs[1, 0].set_title('Atom displacements error')
+axs[0, 1].set_title('Block averaging')
+axs[1, 1].set_title('Standard error')
+
+for i in range(0, 2):
+    for j in range(0, 2):
+        axs[i, j].set_xlabel(xlabel)
+        axs[i, j].set_ylabel(ylabel)
+        axs[i, j].grid()
+
+pl.tight_layout()
+pl.show()
+# pl.savefig('../images/diffusion/diffusion_v_temp_scipy')
+pl.clf()
--- python/diffusionplot.py
+++ python/diffusionplot.py
@@ -1,119 +1,53 @@
-from atom_error_propagation import diffusion as di
-from block_averaging import block_averaging as bl
 from matplotlib import pyplot as pl
 from diffusionimport import load
-from scipy import stats as st
 
-import pandas as pd
-import numpy as np
 import os
 
-directory = '../datacalculated/diffusion/'
+mo = '../datacalculated/diffusion/'
+image = '../images/diffusion/'
 
-runs = os.listdir(directory)
+files = os.listdir(mo)
 
-diffusion = {}
-diffusion['scipy'] = []
-diffusion['block'] = []
-diffusion['var'] = []
-differr = {}
-differr['scipy'] = []
-differr['block'] = []
-differr['fit'] = []
-differr['var'] = []
+names = []
+runset = []
+for item in files:
+    if '_origins' in item:
+        names.append(item)
+        runset.append(item.split('_')[0])
 
-temp = []
-temp2 = []
-for run in runs:
+runset = list(set(runset))
 
-    if '_origins' not in run:
+for run in runset:
+    points = {}
+    for name in names:
+        data = load(mo+name)
 
-        temp.append(int(run.split('run1_')[1]))
+        time = data['start_time']
 
-        with open(directory+run) as file:
-            next(file)
-            for line in file:
-                value = line.strip().split(',')
-                diffusion['scipy'].append(float(value[0]))
-                differr['scipy'].append(float(value[1]))
+        del data['start_time']
 
-    if '_origins' in run:
+        temp = float(name.split('origins_')[1])
+        for key in data:
+            if '_Err' not in key:
+                pl.plot(time, data[key], marker='.', label=key)
 
-        word = run.split('run1_')[1]
-        number = int(word.split('_')[1])
-        temp2.append(number)
+                pl.xlabel('Start Time [ps]')
+                pl.ylabel('Diffusion [*10^-4 cm^2 s^-1]')
+                pl.grid(b=True, which='both')
+                pl.legend(loc='best')
+                pl.tight_layout()
+                pl.savefig(image+name+'_'+key)
+                pl.clf()
 
-        data = load(directory+run, ' ')
-        block = bl(data)
-        diffusion['block'].append(block['all'])
-        differr['block'].append(block['all_err'])
+                if 'all' in key:
+                    points[temp] = data[key]
 
-        diffusion['var'].append(np.mean(data['all']))
-        differr['var'].append((np.var(data['all'])**0.5))
+    for key in points:
+        for item in points[key]:
+            pl.plot(key, item, marker='.', color='b')
 
-directory = '../datacalculated/msd/'
-
-runs = os.listdir(directory)
-
-for run in runs:
-    data = load(directory+run, ',')
-    atom = di(data)
-    differr['fit'].append(atom['all_err'])
-
-fig, axs = pl.subplots(2, 2)
-
-axs[0, 0].errorbar(
-                   temp,
-                   diffusion['scipy'],
-                   differr['scipy'],
-                   marker='.',
-                   linestyle='None',
-                   ecolor='r'
-                   )
-
-axs[1, 0].errorbar(
-                   temp,
-                   diffusion['scipy'],
-                   differr['fit'],
-                   marker='.',
-                   linestyle='None',
-                   ecolor='r'
-                   )
-
-axs[0, 1].errorbar(
-                   temp2,
-                   diffusion['block'],
-                   differr['block'],
-                   marker='.',
-                   linestyle='None',
-                   ecolor='r'
-                   )
-
-axs[1, 1].errorbar(
-                   temp2,
-                   diffusion['var'],
-                   differr['var'],
-                   marker='.',
-                   linestyle='None',
-                   ecolor='r'
-                   )
-
-axs[0, 0].set_xlabel('test')
-xlabel = 'Temperature [K]'
-ylabel = 'Diffusion [*10^-4 cm^2 s^-1]'
-
-axs[0, 0].set_title('Scipy fit error')
-axs[1, 0].set_title('Atom displacements error')
-axs[0, 1].set_title('Block averaging')
-axs[1, 1].set_title('Square root of variance')
-
-for i in range(0, 2):
-    for j in range(0, 2):
-        axs[i, j].set_xlabel(xlabel)
-        axs[i, j].set_ylabel(ylabel)
-        axs[i, j].grid()
-
-pl.tight_layout()
-pl.show()
-# pl.savefig('../images/diffusion/diffusion_v_temp_scipy')
-pl.clf()
+    pl.xlabel('Temperature [K]')
+    pl.ylabel('Diffusion [*10^-4 cm^2 s^-1]')
+    pl.grid(b=True, which='both')
+    pl.tight_layout()
+    pl.savefig(image+run)
