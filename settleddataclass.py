Removed a method that needs to be fixed. Simplified the calculation process.

settleddataclass.py
@@ -8,7 +8,28 @@ class settled(object):
     Class to grab the indexes of settled data based on slopes per bin.
     '''
 
-    def batch(self, x, a=None, b=None):
+    def __init__(self, x, y, a=None, b=None):
+        '''
+        Devide data into a number of bins.
+
+        inputs:
+                x = x-axis data
+                y = y-axis data
+                n = length of x values
+                a = number of bins
+                b = length of bins (approximate)
+                n = length of x values
+        '''
+
+        self.x = x
+        self.y = y
+        self.n = len(x)
+        self.a = a
+        self.b = b
+        self.binselect = {}
+        self.indexes = {}
+
+    def batch(self):
         '''
         Devide data into a number of bins.
 
@@ -20,89 +41,87 @@ class settled(object):
                 a = number of bins
         '''
 
-        n = len(x)
-
         # Estimate the number of bins from block length
-        if a is None:
-            a = n//b
+        if self.a is None:
+            self.a = self.n//self.b
 
         # Estimate the block length from the number of bins
-        if b is None:
-            b = n//a
+        if self.b is None:
+            self.b = self.n//self.a
 
-        blocks = np.array_split(x, a)
+        self.xblocks = np.array_split(self.x, self.a)
+        self.yblocks = np.array_split(self.y, self.a)
 
-        return blocks, a
+        return self.xblocks, self.yblocks
 
-    def binslopes(self, x, y, bins):
+    def binslopes(self):
         '''
         Linear regression for each block of data.
         Data blocks must be same for both x and y.
 
         inputs:
-                x = binned x-axis data
-                y = binned y-axis data
+                self.xblocks = binned x-axis data
+                self.yblocks = binned y-axis data
         outputs:
-                slope = slopes for each bin
+                self.slopes = slopes for each bin
+                self.err = error from fitting
         '''
 
-        slope = []
-        for j in range(0, len(x)):
-            slope.append(np.polyfit(x[j], y[j], 1)[0])
+        slopes = []
+        errs = []
+        for j in range(0, self.a):
+            fit = st.linregress(self.xblocks[j], self.yblocks[j])
+            slopes.append(fit[0])
+            errs.append(fit[-1])
 
-        return slope
+        self.blockslopes = slopes
+        self.errs = errs
 
-    def findslopestart(self, x):
+        return self.blockslopes, self.errs
+
+    def findslopestart(self):
         '''
         Find the index of data where
 
         inputs:
-                x = binned x data
+                self.slopes = binned slope data
         outputs:
                 i = the first slope value fitting criteria
         '''
-        n = len(x)-1
+
+        n = self.n-1
 
         # If the start is decreasing
-        if x[1]-x[0] < 0.0:
+        if self.blockslopes[1]-self.blockslopes[0] < 0.0:
             for i in range(0, n):
-                if x[i+1]-x[i] > 0.0:
+                if self.slopes[i+1]-self.slopes[i] > 0.0:
                     break
 
         # If the start is increasing
-        if x[1]-x[0] > 0.0:
+        if self.blockslopes[1]-self.blockslopes[0] > 0.0:
             for i in range(0, n):
-                if x[i+1]-x[i] < 0.0:
+                if self.blockslopes[i+1]-self.blockslopes[i] < 0.0:
                     break
+
+        self.binselect['slope'] = i
 
         return i
 
-    def finddatastart(self, x, i):
-        '''
-        Find the start of settled data
-
-        inputs:
-                x = binned data
-                i = the bin for settled data
-        outputs:
-                index = the starting index of settled data
-        '''
-        index = sum([len(j) for j in x[:i]])
-
-        return index
-
-    def ptest(self, x, alpha):
+    def ptest(self, alpha=0.05):
         '''
         Find the p-values for each bin with respect to the last bin
 
         inputs:
-                x = binned data
+                self.yblocks = binned y-axis data
                 alpha = the significance level
         outputs:
                 index = The last bin where the p-values is less than alpha
         '''
 
-        pvals = [st.ttest_ind(i, x[-1], equal_var=False)[1] for i in x]
+        pvals = [
+                 st.ttest_ind(i, self.yblocks[-1],
+                 equal_var=False)[1] for i in self.yblocks
+                 ]
 
         count = 0
         for i in pvals:
@@ -111,33 +130,25 @@ class settled(object):
 
             count += 1
 
-        index += 1  # Skip the index that failed
+        if index < self.a-1:
+            index += 1  # Skip the index that failed
+
+        self.binselect['p'] = index
 
         return index
 
-    def slopeoverstd(self, x, m):
+    def finddatastart(self):
         '''
-        Calcualte the slope of bins over their std
+        Find the start of settled data
 
         inputs:
-                x = binned data
-                m = slope of binned data
-                std = std of unbinned data
+                self.yblocks = binned y-axis data
         outputs:
+                index = the starting index of settled data
         '''
 
-        deviations = [np.std(i) for i in x]
-        divisions = [i/j for i, j in zip(m, deviations)]
+        for key in self.binselect:
+            index = sum([len(j) for j in self.yblocks[:self.binselect[key]]])
+            self.indexes[key] = index
 
-        sigma = np.std(divisions)
-
-        count = 0
-        for i in divisions:
-            if i > sigma:
-                index = count
-
-            count += 1
-
-        index += 1  # Skip the index that failed
-
-        return index, divisions
+        return self.indexes